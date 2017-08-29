d0b017d6bdac33482b980295b6b7e0bd775bb415
ceb-student <CEB-Student@caddoschools.org>
2017-08-29 09:17:59 -0500

    Overloaded Constructor

--- BetterJPAMs/src/studentManagment/model/Student.java
+++ BetterJPAMs/src/studentManagment/model/Student.java
@@ -2,13 +2,13 @@ package studentManagment.model;
 
 public class Student 
 {
-	boolean isCurrentStudent;
-	int gradeLevel;
-	String idNumber;
-	String name;
-	String gender;
-	int age;
-	double gpa;
+	private boolean isCurrentStudent;
+	private int gradeLevel;
+	private String idNumber;
+	private String name;
+	private String gender;
+	private int age;
+	private double gpa;
 	
 	public Student()
 	{
@@ -21,4 +21,33 @@ public class Student
 		gpa = 0.00; 
 		
 	}
+	/**
+	 * 
+	 * @param isCurrentStudent 
+	 * @param gradeLevel Student's Grade Level
+	 * @param idNumber
+	 * @param gender
+	 * @param age
+	 * @param gpa
+	 */
+	public Student(boolean isCurrentStudent, int gradeLevel, String idNumber, String gender, int age, double gpa)
+	{
+		this.isCurrentStudent = isCurrentStudent;
+		this.gradeLevel = gradeLevel;
+		this.idNumber = idNumber;
+		this.gender = gender;
+		this.age = age;
+		this.gpa = gpa;
+	}
+	
+	public String toString()
+	{
+		String str = "This person is not a current student.";
+		if (isCurrentStudent) 
+		{
+		str = "Name: " + name + " " + "IDnumber:" + idNumber + " " + "Gender: " + gender + " " + "Age:" + age + " " + "GPA:" + gpa + " "+ "Grade:" + gradeLevel + " ";    
+		}
+		return str;
+	}
+	
 }
