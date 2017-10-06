35d41d3f6299150f8149b53f2771c569bb894509
ceb-student <CEB-Student@caddoschools.org>
2017-10-06 08:47:33 -0500

    Finished Die class and added other classes from student files
    finished class for the die roll and added the other classes from the student files

--- Craps/src/Die.java
+++ Craps/src/Die.java
@@ -1,4 +1,23 @@
 
-public class Die {
-
+public class Die 
+{
+	
+	private int NumDots;
+	
+	public void roll ()
+	{
+		NumDots = (int)(Math.random() *6)+1;
+	}
+	
+	public int getNumDots()
+	{
+		return NumDots;
+	}
+	
+	
+	
+	
+	
+	
+	
 }
